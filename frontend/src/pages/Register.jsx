bd2b7951f84bf1a9bcdd5e8af53752c9cda03966
Hussain Jamil <hosen3919@gmail.com>
2026-09-11 14:15:30 +0300

    Connect registration form to API

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,110 @@
+import {Link, useNavigate} from "react-router-dom";
+import {useState} from "react";
+import api from "../services/api.js";
+
 export default function Register() {
+    const navigate = useNavigate();
+    // register form state
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    });
+
+    const handleChange =(e) =>{
+        const {name, value} = e.target;
+        setFormData(prevData =>({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+
+        if(formData.password !== formData.confirmPassword) {
+            alert("Passwords don't match!");
+            return;
+        }
+
+        // hadel a registration form submission
+        try {
+            await api.post("/register", {
+                name: formData.name,
+                email: formData.email,
+                password: formData.password,
+            });
+
+            alert("Registered successfully!");
+
+            navigate("/login");
+
+        } catch(err) {
+            console.error(err);
+            // backend error message
+            const message = err.response?.data?.detail || "Registration failed.";
+            alert(message);
+        }
+    };
+
+
     return (
-        <div>
-            <h1>Register</h1>
+        <div className="container min-vh-100 d-flex justify-content-center align-items-center">
+            <div style={{background:"#EFF1F3", maxWidth: "600px"}} className="container-fluid d-flex flex-column justify-content-center align-items-center p-5 rounded-5">
+                <i className="bi bi-calendar2-week fs-1 eventhub-color"></i>
+                <h1 className="eventhub-color fw-bold">EventHub</h1>
+                <p>Professional Event Management Portal</p>
+
+                {/* form registration */}
+                <form onSubmit={handleSubmit} className="w-100">
+                    <div className="mb-3">
+                        <label className="form-label fw-medium ">Full Name:</label>
+                        <input type="text" className="form-control" placeholder="Enter Your Full  Name"
+                            name="name" required
+                            value={formData.name}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label fw-medium">Email:</label>
+                        <input type="email" className="form-control" placeholder="Enter Your Email"
+                            name="email" required
+                            value={formData.email}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label fw-medium">Password:</label>
+                        <input type="password" className="form-control" placeholder="Enter Your Password"
+                            name="password" required
+                            value={formData.password}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label className="form-label fw-medium">Confirm Password:</label>
+                        <input type="password" className="form-control" placeholder="Confirm Your Password"
+                            name="confirmPassword" required
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
+                        />
+                    </div>
+
+                    <button type="submit" className="btn eventhub-button w-100">Register</button>
+                </form>
+     
+                <hr></hr>
+                <p className="text-center mt-3">
+                    Already have an account? 
+                    <Link to="/login">Sign In</Link>
+                </p>
+                                
+            </div>
+       
         </div>
     );
 }
